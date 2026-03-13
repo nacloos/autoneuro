dc3532e6122325bcca73df2d776d0a105c992d0b
nacloos <cloosnathan@gmail.com>
2026-03-13 07:25:30 -0400

    exp: K_cmp=2 (two comparators) + EMA-SWA 0.98

--- model.py
+++ model.py
@@ -847,8 +847,8 @@ def init_modular_params(
 
     K_sg = 1  # Always add 1 sensory gating module
     K_li = 1  # Always add 1 lateral inhibition module
-    K_cmp = 1  # Always add 1 comparator module
-    K_gru = 2  # Add 2 GRU modules for more capacity
+    K_cmp = 2  # Add 2 comparator modules
+    K_gru = 1  # Always add 1 GRU module
     K_res = 1  # Always add 1 reservoir module
     total_modules = K_int + K_mem + K_sg + K_li + K_cmp + K_gru + K_res
     n_selections = total_modules + 1  # +1 for null module
